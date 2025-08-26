8f01a4a419cc2f88ff781c1bb1b9e5180d574f4f
soumario <mario.aaraujojr@sptech.school>
2025-08-26 19:52:40 -0300

    New - Estrutura do projeto

---
+++ cr-metais/src/main/java/school/sptech/cr_metais/CrMetaisApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.cr_metais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrMetaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrMetaisApplication.class, args);
+	}
+
+}
---
+++ cr-metais/src/test/java/school/sptech/cr_metais/CrMetaisApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.cr_metais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrMetaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
