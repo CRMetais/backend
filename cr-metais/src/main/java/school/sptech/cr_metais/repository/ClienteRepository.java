0ef90a9fb5083c5658735f939645d388cfd443ed
enricodmr <enrico.damaro@sptech.school>
2026-05-18 13:39:05 -0300

    a

--- cr-metais/src/main/java/school/sptech/cr_metais/repository/ClienteRepository.java
+++ cr-metais/src/main/java/school/sptech/cr_metais/repository/ClienteRepository.java
@@ -1,8 +1,8 @@
-package school.sptech.cr_metais.repository;
+    package school.sptech.cr_metais.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import school.sptech.cr_metais.entity.Cliente;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import school.sptech.cr_metais.entity.Cliente;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
+    public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    }
 
